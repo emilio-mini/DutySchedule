ec747161035ef59ae181e2de454f48feb6957b11
emilio.mini <dev@emilio-mini.me>
2025-08-28 16:27:52 +0200

    Added duty card carrousels; Split view into day and night-shift

--- app/src/main/java/me/emiliomini/dutyschedule/models/prep/TimelineItem.kt
+++
@@ -1,8 +0,0 @@
-package me.emiliomini.dutyschedule.models.prep
-
-import java.time.OffsetDateTime
-
-sealed interface TimelineItem {
-    data class Date(val date: OffsetDateTime) : TimelineItem
-    data class Duty(val duty: DutyDefinition) : TimelineItem
-}
