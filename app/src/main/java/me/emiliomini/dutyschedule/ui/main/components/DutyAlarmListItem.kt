17f863d9d4a4e85cc339ab620489dbfec007d5cd
emilio.mini <dev@emilio-mini.me>
2025-09-17 20:58:02 +0200

    Improved time picker dialog

--- app/src/main/java/me/emiliomini/dutyschedule/ui/main/components/DutyAlarmListItem.kt
+++ app/src/main/java/me/emiliomini/dutyschedule/ui/main/components/DutyAlarmListItem.kt
@@ -6,14 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Alarm
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,13 +27,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import me.emiliomini.dutyschedule.R
 import me.emiliomini.dutyschedule.services.storage.DataKeys
 import me.emiliomini.dutyschedule.services.storage.StorageService
 import me.emiliomini.dutyschedule.ui.components.CardListItem
+import me.emiliomini.dutyschedule.ui.components.WheelSelector
 import kotlin.math.abs
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -103,48 +101,51 @@ fun DutyAlarmListItem(modifier: Modifier = Modifier) {
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
                 ) {
-                    OutlinedTextField(
-                        modifier = Modifier.width(92.dp),
-                        value = hour,
-                        placeholder = { Text("00") },
-                        textStyle = MaterialTheme.typography.headlineLarge,
-                        onValueChange = { newHour ->
-                            if (newHour.length <= 2 && newHour.all { it.isDigit() }) {
-                                if (newHour.isNotBlank() && newHour.toInt() > 24) {
-                                    return@OutlinedTextField
-                                }
-
-                                hour = newHour
+                    WheelSelector(
+                        modifier = Modifier.width(48.dp),
+                        items = (0..23).map {
+                            var str = it.toString()
+                            while (str.length < 2) {
+                                str = "0$str"
                             }
+                            str
                         },
-                        label = { Text(stringResource(R.string.main_settings_alarms_pre_event_begin_dialog_hour)) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        singleLine = true
+                        selectedColor = MaterialTheme.colorScheme.primary,
+                        onValueChange = {
+                            hour = it.toString()
+                        },
+                        selectedIndex = hour.toInt(),
+                        textStyle = MaterialTheme.typography.headlineLarge.copy(fontFeatureSettings = "tnum"),
+                        alignment = Alignment.End,
+                        monospaced = true,
+                        monospacedLetterWidth = 24.dp
                     )
                     Text(
                         ":",
                         modifier = Modifier
-                            .padding(top = 6.dp)
+                            .padding(bottom = 6.dp)
                             .padding(horizontal = 8.dp),
-                        style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold)
+                        style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold),
+                        color = MaterialTheme.colorScheme.primary
                     )
-                    OutlinedTextField(
-                        modifier = Modifier.width(92.dp),
-                        value = minute,
-                        placeholder = { Text("00") },
-                        textStyle = MaterialTheme.typography.headlineLarge,
-                        onValueChange = { newMin ->
-                            if (newMin.length <= 2 && newMin.all { it.isDigit() }) {
-                                if (newMin.isNotBlank() && newMin.toInt() > 60) {
-                                    return@OutlinedTextField
-                                }
-
-                                minute = newMin
+                    WheelSelector(
+                        modifier = Modifier.width(48.dp),
+                        items = (0..59).map {
+                            var str = it.toString()
+                            while (str.length < 2) {
+                                str = "0$str"
                             }
+                            str
                         },
-                        label = { Text(stringResource(R.string.main_settings_alarms_pre_event_begin_dialog_minute)) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        singleLine = true
+                        selectedColor = MaterialTheme.colorScheme.primary,
+                        onValueChange = {
+                            minute = it.toString()
+                        },
+                        selectedIndex = minute.toInt(),
+                        textStyle = MaterialTheme.typography.headlineLarge.copy(fontFeatureSettings = "tnum"),
+                        alignment = Alignment.Start,
+                        monospaced = true,
+                        monospacedLetterWidth = 24.dp
                     )
                 }
             },
